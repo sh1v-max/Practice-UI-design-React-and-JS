a8a92c8c23321f2c6e224a9d4a983cf3a74ea6ba
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-07 20:38:40 +0530

    image carousel

--- 09-Image-carousel/src/ImageCarousel.jsx
+++ 09-Image-carousel/src/ImageCarousel.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from 'react'
 import data from './data.json'
 const DATA_LENGTH = data.length
-console.log(data)
+// console.log(data)
 
 export default function ImageCarousel() {
   const [index, setIndex] = useState(0)
   const ref = useRef(null)
-  let intervalId
 
   const handleNext = () => {
     setIndex((prevIndex) => {
@@ -34,18 +33,27 @@ export default function ImageCarousel() {
   }, [])
 
   return (
-    <div>
+    <div className="carousel-wrapper">
+      <div className="carousel-header">
+        <h2 className="carousel-title">Image Gallery</h2>
+        <p className="carousel-subtitle">Discover beautiful moments captured in time</p>
+      </div>
+      
       <div
         onMouseEnter={() => clearInterval(ref.current)}
         onMouseLeave={() => {ref.current = setInterval(handleNext, 1000)}}
         className="container"
       >
         <div onClick={handlePrev} className="left-btn">
-          {'<'}
+          &#8249;
         </div>
-        <img src={data[index].download_url} alt="" />
+        <img src={data[index].download_url} alt={`Photo by ${data[index].author}`} className="carousel-image" />
         <div onClick={handleNext} className="right-btn">
-          {'>'}
+          &#8250;
+        </div>
+        
+        <div className="carousel-info">
+          <span className="image-author">Photo by {data[index].author}</span>
         </div>
       </div>
     </div>
