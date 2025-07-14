b5e0f90b0288c69b278ba6e038b10ffd41b15a95
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-14 17:32:17 +0530

    interactive grid

--- 14-Interactive-grid/src/Components/InteractiveGrid.jsx
+++ 14-Interactive-grid/src/Components/InteractiveGrid.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const InteractiveGrid = () => {
+  const [grid, setGrid] = useState(
+    Array.from({ length: 3 }, () => new Array(3).fill(false))
+  )
+  console.log(grid)
+
   return (
-    <div>InteractiveGrid</div>
+    <div className="container">
+      {grid.map((row, rowIdx) => {
+        return row.map((cell, colIdx) => {
+          return <div className="cell" key={`${rowIdx}-${colIdx}`}></div>
+        })
+      })}
+    </div>
   )
 }
 
