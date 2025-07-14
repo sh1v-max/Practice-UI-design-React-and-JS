82f36d841bbc15f905d1ec4dda5938d97ae222aa
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-14 17:00:08 +0530

    interactive grid

---
+++ 14-Interactive-grid/src/App.js
@@ -0,0 +1,8 @@
+import InteractiveGrid from "./Components/InteractiveGrid";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <InteractiveGrid/>
+  );
+}
---
+++ 14-Interactive-grid/src/Components/InteractiveGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InteractiveGrid = () => {
+  return (
+    <div>InteractiveGrid</div>
+  )
+}
+
+export default InteractiveGrid
