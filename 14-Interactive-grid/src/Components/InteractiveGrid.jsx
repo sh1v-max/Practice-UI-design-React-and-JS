2fae85eadf4313130d9a5f01bc4c9f2a03463199
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-14 19:06:50 +0530

    interactive grid

--- 14-Interactive-grid/src/Components/InteractiveGrid.jsx
+++ 14-Interactive-grid/src/Components/InteractiveGrid.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const InteractiveGrid = () => {
   const [grid, setGrid] = useState(
@@ -6,11 +6,53 @@ const InteractiveGrid = () => {
   )
   console.log(grid)
 
+  // now we need something, a variable to store the order of the cells
+  // now, we can not create const queue = [], because it will be overwritten by each render as empty array
+  // we can't use state either, because we need state only when we want to change the UI based on state change
+  // so, now we will use ref
+  const queue = useRef([])
+  console.log(queue)
+
+  const handleOnClick = (rowIdx, colIdx, flag) => {
+    if(grid[rowIdx][colIdx] && flag) {
+      return
+    }
+    // const gridDeepCopy = grid.map((row) => [...row])
+    // gridDeepCopy[rowIdx][colIdx] = flag
+    // if (flag) queue.current.push([rowIdx, colIdx])
+    // setGrid(gridDeepCopy)
+    // there is still a bug, we're not getting updated value of grid
+    // to get updated value of grid, we will pass a callback inside setGrid
+    setGrid((prevGrid) => {
+      const gridDeepCopy = prevGrid.map((row) => [...row])
+      gridDeepCopy[rowIdx][colIdx] = flag
+      if (flag) queue.current.push([rowIdx, colIdx])
+      return gridDeepCopy
+    })
+  }
+
+  useEffect(() => {
+    if (queue.current.length === 9) {
+      queue.current.forEach(([rowIdx, colIdx], idx) => {
+        setTimeout(() => handleOnClick(rowIdx, colIdx, false), 1000 * (idx + 1))
+      })
+      // console.log('start removing color')
+      // to prevent timer from running again and again and...
+      queue.current = []
+    }
+  })
+
   return (
     <div className="container">
       {grid.map((row, rowIdx) => {
         return row.map((cell, colIdx) => {
-          return <div className="cell" key={`${rowIdx}-${colIdx}`}></div>
+          return (
+            <div
+              className={`cell ${cell ? 'active' : ''}`}
+              key={`${rowIdx}-${colIdx}`}
+              onClick={() => handleOnClick(rowIdx, colIdx, true)}
+            ></div>
+          )
         })
       })}
     </div>
