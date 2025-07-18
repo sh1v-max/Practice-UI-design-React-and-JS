d7c23fdf0834be5199ba0fdfd414b99f82ecae02
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-18 20:33:03 +0530

    virtualization using react doone

--- 18-React-virtualisation-list/src/App.js
+++ 18-React-virtualisation-list/src/App.js
@@ -6,11 +6,23 @@ const LIST = Array.from({ length: 10000 }, (_, i) => i + 1)
 
 export default function App() {
   return (
-    <VirtualizedList 
-      list={LIST} 
-      height={400} 
-      width={300} 
-      itemHeight={35} 
-    />
+    <div className="app">
+      <div className="app-header">
+        <h1 className="app-title">React Virtualization Demo</h1>
+        <p className="app-description">
+          A simple virtualized list implementation that efficiently renders 10,000 items 
+          by only displaying visible elements in the viewport. Scroll to see the magic!
+        </p>
+      </div>
+      
+      <div className="virtualized-container">
+        <VirtualizedList 
+          list={LIST} 
+          height={400} 
+          width={300} 
+          itemHeight={35} 
+        />
+      </div>
+    </div>
   )
 }
--- 18-React-virtualisation-list/src/Components/VirtualizedList.jsx
+++ 18-React-virtualisation-list/src/Components/VirtualizedList.jsx
@@ -6,6 +6,7 @@ const VirtualizedList = ({ list, height, width, itemHeight }) => {
   // visibleList will be updated based on new indices
   const visibleList = list.slice(indices[0], indices[1] + 1)
   const handleScroll = (e) => {
+    // this is similar to sliding window concept
     const {scrollTop} = e.target
     console.log(scrollTop)
     const newStartIndex = Math.floor(scrollTop / itemHeight);
@@ -17,22 +18,20 @@ const VirtualizedList = ({ list, height, width, itemHeight }) => {
     <div
       className="container"
       onScroll={handleScroll}
-      style={{ height, width, background: 'grey', overflow: 'auto' }}
+      style={{ height, width, overflow: 'auto' }}
     >
+      {/* giving parent div a full height for scrolling */}
       <div style={{ height: list.length * itemHeight, position: 'relative' }}>
         {visibleList.map((item, index) => {
           return (
             <div
+              key={item}
               className="item"
               style={{
                 height: itemHeight,
-                background: 'coral',
-                borderTop: '5px solid grey',
                 position: 'absolute',
                 top: (indices[0] + index) * itemHeight,
                 width: '100%',
-                textAlign: 'center',
-                // color: 'whitesmoke',
               }}
             >
               {'Item ' + item}
