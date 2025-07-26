eaf1b4ddc9c1a382bc5d2345f8c8d4c36597f480
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-26 20:13:37 +0530

    logic done

--- 24-Crypto.jsx/src/Components/Crypto.jsx
+++ 24-Crypto.jsx/src/Components/Crypto.jsx
@@ -1,32 +1,74 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+const CURRENCY_OPTIONS = ['USD', 'EUR', 'GBP', 'CNY', 'JPY']
+const CRYPTO_API = 'https://api.frontendeval.com/fake/crypto/'
 
 const Crypto = () => {
-  const currencyOptions = ['USD', 'EUR', 'GBP', 'CNY']
-  const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState(localStorage.getItem('amount') || 0)
   const [convertedAmount, setConvertedAmount] = useState(0)
-  
+  const [conversionRate, setConversionRate] = useState(0)
+  const [prevConvertedAmt, setPrevConvertedAmt] = useState(0)
+  const [currency, setCurrency] = useState(
+    localStorage.getItem('currency') || CURRENCY_OPTIONS[0]
+  )
+  const priceChange = convertedAmount - prevConvertedAmt
+
+  useEffect(() => {
+    localStorage.setItem('amount', amount)
+    localStorage.setItem('currency', currency)
+  }, [amount, currency])
+
+  useEffect(() => {
+    async function fetchConversionRate() {
+      try {
+        const res = await fetch(`${CRYPTO_API}${currency}`)
+        const data = await res.json()
+        // console.log(data)
+        setConversionRate(data.value)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchConversionRate()
+
+    const timer = setInterval(fetchConversionRate, 2000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [currency])
+
+  useEffect(() => {
+    setPrevConvertedAmt(convertedAmount)
+    setConvertedAmount(amount * conversionRate)
+  }, [conversionRate])
+
   return (
     <div>
       <label htmlFor="amountToConvert">
-        Amount to Convert : 
-        <input type="text" 
-        id='amountToConvert'
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}/>
+        Amount to Convert :
+        <input
+          type="text"
+          id="amountToConvert"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </label>
 
       <label htmlFor="currency">
-        Currency : 
-        <select id="currency">
-          {currencyOptions.map((value, index) => {
-            return <option key={index} value={value}>
-              {value}
-            </option>}
-          )}
+        Currency :
+        <select id="currency" onChange={(e) => setCurrency(e.target.value)}>
+          {CURRENCY_OPTIONS.map((value, index) => {
+            return (
+              <option key={index} value={value}>
+                {value}
+              </option>
+            )
+          })}
         </select>
       </label>
-      <p>STRIFF Crypto Equivalent: {convertedAmount}</p>
-      <p className='change'>Change : ⬆️ 0.5</p>
+      <p>STRIFF Crypto Equivalent: {convertedAmount.toFixed(3)}</p>
+      <p className={`change ${priceChange > 0 ? 'green' : 'red'}`}>
+        Change : {priceChange > 0 ? '⬆️' : '⬇️'} {priceChange.toFixed(3)}
+      </p>
     </div>
   )
 }
