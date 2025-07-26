46fbb08247e42bd2bd70cb50a37df92222a5701d
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-26 19:04:52 +0530

    calling api

--- 24-Crypto.jsx/src/Components/Crypto.jsx
+++ 24-Crypto.jsx/src/Components/Crypto.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Crypto = () => {
+  const currencyOptions = ['USD', 'EUR', 'GBP', 'CNY']
+  const [amount, setAmount] = useState(0)
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  
   return (
-    <div>Crypto</div>
+    <div>
+      <label htmlFor="amountToConvert">
+        Amount to Convert : 
+        <input type="text" 
+        id='amountToConvert'
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}/>
+      </label>
+
+      <label htmlFor="currency">
+        Currency : 
+        <select id="currency">
+          {currencyOptions.map((value, index) => {
+            return <option key={index} value={value}>
+              {value}
+            </option>}
+          )}
+        </select>
+      </label>
+      <p>STRIFF Crypto Equivalent: {convertedAmount}</p>
+      <p className='change'>Change : ⬆️ 0.5</p>
+    </div>
   )
 }
 
