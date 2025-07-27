7bd289bd83137918727fe59770a0f770324662a6
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-27 20:19:05 +0530

    logic done, interval cleared

--- 25-Typing-effect/src/Components/TypingEffect.jsx
+++ 25-Typing-effect/src/Components/TypingEffect.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react'
 
+// goal:
+// know how to use timer
+// also when and how to clean up the timer
+// it will exhaust browser's memory and crash
+
 export const TypingEffect = ({ text, delay }) => {
-  const [displayText, setDisplayText] = useState(text.slice(0, 10));
-  const velocityRef = useRef({speed: 1, endIndex: 0})
+  // using slice to move the cursor
+  const [displayText, setDisplayText] = useState(text.slice(0, 10))
+  const velocityRef = useRef({ speed: 1, endIndex: 0 })
   useEffect(() => {
-    
+    const interval = setInterval(() => {
+      if (velocityRef.current.endIndex === text.length) {
+        velocityRef.current.speed = -1
+      } else if (velocityRef.current.endIndex === 0) {
+        velocityRef.current.speed = 1
+      }
+      velocityRef.current.endIndex += velocityRef.current.speed
+      setDisplayText(text.slice(0, velocityRef.current.endIndex))
+    }, delay)
+
+    return () => {
+      clearInterval(interval)
+    }
   }, [delay, text])
-  
+
   console.log(text, delay)
   return (
     <div>
