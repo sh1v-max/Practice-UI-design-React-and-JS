a1250fc17dbd85b05d613f89e0d33e0ed72c9483
sh1v-max <singhshiv0427@gmail.com>
2025-09-19 12:27:11 +0530

    updated chart using canvasJS

--- 45-Admin-dashboard-2.0/src/Components/Dashboard.tsx
+++ 45-Admin-dashboard-2.0/src/Components/Dashboard.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import Footer from './Footer.tsx'
 import Header from './Header.tsx'
 import StatusBar from './StatusBar.tsx'
 import Main from './Main.tsx'
+import Footer from './Footer.tsx'
 
 const Dashboard: React.FC = () => {
   return (
-    <div className='bg-white min-h-screen '>
+    <div className="bg-white min-h-screen ">
       <div className="min-h-screen w-full md:w-[500px] border-2 border-gray-300 rounded-t-2xl rounded-b-[46px] mx-auto">
         {/* Header */}
         <StatusBar />
         <Header />
         {/* Main Content */}
         <Main />
-        <Footer />
+        <Footer/>
       </div>
     </div>
   )
