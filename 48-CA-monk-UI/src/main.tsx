ceef3fd9a2f978bf48b5a58036678b0f501ce08e
sh1v-max <singhshiv0427@gmail.com>
2026-01-17 16:25:05 +0530

    initialized tanstack to my app

--- 48-CA-monk-UI/src/main.tsx
+++ 48-CA-monk-UI/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 
+// this creates a client that will be used by react query to store cache and other data like fetch and all
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* queryClientProvider takes queryClient as a prop which acts as client */}
+    {/* this works like redux provider or context provider, yes you've used them before dummy */}
+    <QueryClientProvider client ={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 )
+
+// a simple visual representation of how tanstack react query works
+// QueryClient
+//    |
+// QueryClientProvider
+//    |
+// App
+//    |
+// BlogList / BlogDetail
+//    |
+// useQuery(...)
