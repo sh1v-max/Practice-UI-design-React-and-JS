4f46749c52516caa56567551a9e0cc5bcdf88ae1
sh1v-max <singhshiv0427@gmail.com>
2026-01-17 18:05:33 +0530

    installed shadcn ui and button

--- 48-CA-monk-UI/src/App.tsx
+++ 48-CA-monk-UI/src/App.tsx
@@ -1,10 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import { Button } from './components/ui/button'
 import './App.css'
 
 function App() {
+  useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      return res.json()
+    },
+  })
 
   return (
     <>
-    <h1 className = "text-3xl font-bold underline">Welcome home</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button onClick={() => console.log("clicked")} >Click Me</Button>
+      </div>
     </>
   )
 }
